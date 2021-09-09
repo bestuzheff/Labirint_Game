1ade3a2d729ed6ea5fb14cbc47c213dda596d5db
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2021-09-09 16:49:22 +0300

    Initial commit

---
+++ labirint.py
@@ -0,0 +1,51 @@
+import pygame
+
+size = widht, height = (960, 960)
+screen = pygame.display.set_mode(size)
+
+labirint = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+def load_image(image_file_name):
+    full_image_file_name = 'images' + '/' + image_file_name
+    try:
+        image = pygame.image.load(full_image_file_name).convert_alpha()
+        # image = pygame.transform.scale(image, (64, 64)).convert() #Изменение размера картинки
+    except any:
+        print("Ошибка загрузка изображения:", image_file_name)
+        raise SystemExit()
+    return image
+
+
+white = load_image("white.png")
+black = load_image("black.png")
+
+for x in range(15):
+    for y in range(15):
+        if labirint[y][x] == 0:
+            screen.blit(white, (x * 64, y * 64))
+        else:
+            screen.blit(black, (x * 64, y * 64))
+
+pygame.init()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
